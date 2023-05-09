bc33162c1081825eeae350771e0f83e8c640a653
annie conron <s3900816@student.rmit.edu.au>
2023-05-09 16:41:43 +1000

    style

--- blog.tsx
+++ blog.tsx
@@ -7,7 +7,7 @@ blog({
   description: "This is my coding blog where I document my exploration of creative coding.",
   header: <header>Annie Conron</header>,
   // section: <section>Your custom section</section>,
-  footer: <footer>Email: s3900816@student.RMIT.edu.au</footer>,
+  //footer: <footer>Email: s3900816@student.RMIT.edu.au</footer>,
   avatar: "./images/flower.png",
   avatarClass: "rounded-full",
   author: "Annie Conron",
